c4301290bbfea83f62c5ef512a50d8fd91ca7287
Laurent Dezo <laurentdezo@gmail.com>
2021-04-27 10:27:06 -0400

    Create board state function and accessor functions

--- index.rb
+++ index.rb
@@ -10,6 +10,30 @@ class GameBoard
     @bottom_center = " "
     @bottom_right = " "
   end
+
+  def print_state
+    puts board_state
+  end
+
+  private 
+
+  attr_accessor :top_left, :top_center, :top_right, :center_left, :center, :center_right, :bottom_left, :bottom_center, :bottom_right
+
+   def board_state
+    "   |   |   \n" +
+    " #{top_left} | #{top_center} | #{top_right} \n" +
+    "___|___|___\n" +
+    "   |   |   \n" +
+    " #{center_left} | #{center} | #{center_right} \n" +
+    "___|___|___\n" +
+    "   |   |   \n" +
+    " #{bottom_left} | #{bottom_center} | #{bottom_right} \n" +
+    "   |   |   "
+   end
 end
+
+game = GameBoard.new
+
+game.print_state
 
   
