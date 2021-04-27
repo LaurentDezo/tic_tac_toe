ec0aafe8456e784e6840f577af7dede2cd81b306
Laurent Dezo <laurentdezo@gmail.com>
2021-04-27 10:08:29 -0400

    Create GameBoard class and make a variable for each square when initialized

---
+++ index.rb
@@ -0,0 +1,15 @@
+class GameBoard
+  def initialize
+    @top_left = " "
+    @top_center = " "
+    @top_right = " "
+    @center_left = " "
+    @center = " "
+    @center_right = " "
+    @bottom_left = " "
+    @bottom_center = " "
+    @bottom_right = " "
+  end
+end
+
+  
