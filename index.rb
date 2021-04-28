8870b88035be10afa201ab5dabb9a52920c2b4c5
Laurent Dezo <laurentdezo@gmail.com>
2021-04-28 09:44:47 -0400

    Create swuare class and it's chec method

--- index.rb
+++ index.rb
@@ -1,39 +1,57 @@
+require "pry"
+
 class GameBoard
   def initialize
     @top_left = " "
-    @top_center = " "
+    @top = " "
     @top_right = " "
     @center_left = " "
     @center = " "
     @center_right = " "
     @bottom_left = " "
-    @bottom_center = " "
+    @bottom = " "
     @bottom_right = " "
   end
 
   def print_state
+    self.top_left=("X")
+    self.center=("O")
     puts board_state
   end
 
   private 
-
-  attr_accessor :top_left, :top_center, :top_right, :center_left, :center, :center_right, :bottom_left, :bottom_center, :bottom_right
+  attr_accessor :top_left, :top, :top_right, :center_left, :center, :center_right, :bottom_left, :bottom, :bottom_right
 
    def board_state
     "   |   |   \n" +
-    " #{top_left} | #{top_center} | #{top_right} \n" +
+    " #{top_left} | #{top} | #{top_right} \n" +
     "___|___|___\n" +
     "   |   |   \n" +
     " #{center_left} | #{center} | #{center_right} \n" +
     "___|___|___\n" +
     "   |   |   \n" +
-    " #{bottom_left} | #{bottom_center} | #{bottom_right} \n" +
+    " #{bottom_left} | #{bottom} | #{bottom_right} \n" +
     "   |   |   "
    end
 end
 
-game = GameBoard.new
+class Square
+  attr_reader :content
+  def initialize
+    @content = " " 
+  end
 
-game.print_state
+  def check
+    self.content=("X")
+  end
+
+  private
+  attr_writer :content
+end
+
+square = Square.new
+puts square.content
+square.check
+puts square.content
 
   
